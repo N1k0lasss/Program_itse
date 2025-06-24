28934e6a01347316fa67c21c9de3556d4ce0bcc0
nikkilazarte <nicolas.lazarte798@gmail.com>
2025-06-24 09:47:59 -0300

    update

--- Vectores/vectores-diagrama-codigo/10.py
+++ Vectores/vectores-diagrama-codigo/10.py
@@ -2,3 +2,10 @@ vector = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 vector.append(57)
 vector.sort()
 print(vector)
+
+# Ejemplo con input
+# vector = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# number = int(input("ingrese un numero: "))
+# vector.append(number)
+# vector.sort()
+# print(vector)
